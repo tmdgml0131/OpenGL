f2374e411aa6bdc70a984ff6fb47665ebac9959c
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-07-24 21:33:24 +0900

    OpenGl
    Rotating

--- Learning/OpenGLLearning/main.cpp
+++ Learning/OpenGLLearning/main.cpp
@@ -11,13 +11,16 @@ using namespace std;
 
 // Window Dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
 GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.9f;
-float triIncrement = 0.05f;
+float triIncrement = 0.01f;
+
+float curAngle = 0.0f;
 
 
 // Vertex Shader
@@ -205,6 +208,10 @@ int main()
 		if (abs(triOffset) >= triMaxoffset)
 			direction = !direction;
 
+		curAngle += 0.05f;
+		if (curAngle >= 360)
+			curAngle -= 360;
+
 		// Clear Window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -212,6 +219,7 @@ int main()
 		glUseProgram(shader);
 
 		glm::mat4 model;
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
 		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));

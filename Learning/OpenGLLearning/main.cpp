25c57251044380c7b6d1ef3adfbbdc176cfc6f78
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-08-22 15:22:32 +0900

    opengl
    opengl

--- Learning/OpenGLLearning/main.cpp
+++ Learning/OpenGLLearning/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, shader, IBO, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -35,11 +35,12 @@ layout (location = 0) in vec3 pos;													\n\
 																					\n\
 uniform mat4 model;																	\n\
 																					\n\
-																					\n\
+out vec4 vCol;																		\n\
 																					\n\
 void main()																			\n\
 {																					\n\
 	gl_Position = model * vec4(pos, 1.0);											\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);										\n\
 }																					\n\
 ";
 
@@ -47,24 +48,37 @@ void main()																			\n\
 static const char* fShader = "							\n\
 #version 330											\n\
 														\n\
+in vec4 vCol;											\n\
+														\n\
 out vec4 color;											\n\
 														\n\
 void main()												\n\
 {														\n\
-	color = vec4(0.0, 1.0, 0.0, 1.0);					\n\
+	color = vCol;										\n\
 }														\n\
 ";
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0
+	};
+
 	GLfloat vertices[] = {
-		-0.3f, -0.3f, 0.0f,
-		0.3f, -0.3f, 0.0f,
-		0.0f, 0.3f, 0.0f
+		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -74,6 +88,7 @@ void CreateTriangle()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 }
@@ -191,6 +206,8 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -228,21 +245,23 @@ int main()
 
 		// Clear Window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model;
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 
 		// Scale은 다른 함수에 영향을 줄 수 있기 때문에 보통 translate과 rotation 뒤에 한다.
-		model = glm::scale(model, glm::vec3(0.4f, curSIze, 1.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
 		glUseProgram(0);
